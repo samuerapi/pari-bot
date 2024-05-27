bd13fa7c27ea40591eb71cd065889f8d3abdca50
Student <roventforch@bk.ru>
2024-05-27 13:43:06 +0300

    ivan

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 import asyncio
 
+
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters.state import StateFilter
+from UserStates import UserStates
+from keyboardhelper import get_keyboard
+
 from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(Command("start"))
+async def send_welcome(message: types.Message, state: FSMContext):
+    kb = get_keyboard(["/test"])
+    await message.answer('Привет! Я пари-бот!', reply_markup=kb)
+    await state.set_state(UserStates.BASE)
+
+
+@dp.message(Command("test"), StateFilter(UserStates.BASE))
 async def send_welcome(message: types.Message):
-    await bot.send_message(message.chat.id, "Привет! Я эхо-бот! ☺☻♥")
+    await message.answer('Я в состоянии BASE')
 
 
 async def main():
     await dp.start_polling(bot)
-
 
 if __name__ == '__main__':
     asyncio.run(main())
